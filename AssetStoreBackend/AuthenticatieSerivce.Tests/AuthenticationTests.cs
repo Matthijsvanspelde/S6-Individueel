f46ca3101eaeb287a07c56f078ec5674733fe0fe
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2022-06-08 15:16:25 +0200

    Add security tests

--- AssetStoreBackend/AuthenticatieSerivce.Tests/AuthenticationTests.cs
+++ AssetStoreBackend/AuthenticatieSerivce.Tests/AuthenticationTests.cs
@@ -1,5 +1,8 @@
+using AuthenticationService.Dtos;
 using Microsoft.AspNetCore.Mvc.Testing;
+using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -8,13 +11,45 @@ namespace AuthenticatieSerivce.Tests
     public class AuthenticationTests
     {
         [Fact]
-        public async Task GET_returns_Unauthorized()
+        public async Task GivenMissingAuthorization_ThenUnauthorized()
         {
             using var application = new WebApplicationFactory<AuthenticationService.Startup>();
             using var client = application.CreateClient();
 
             var response = await client.GetAsync("/api/test");
             Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+        
+        [Fact]
+        public async Task GivenInvalidInput_ThenBadRequest()
+        {
+            using var application = new WebApplicationFactory<AuthenticationService.Startup>();
+            using var client = application.CreateClient();
+
+            var formContent = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("password", "Pa55w0rd!"),
+                new KeyValuePair<string, string>("email", "JohnDoe@example.com")
+            });
+
+            var response = await client.PostAsync("/api/authentication/register", formContent);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+        [Fact]
+        public async Task GivenInvalidPassword_ThenInternalServerError()
+        {
+            using var application = new WebApplicationFactory<AuthenticationService.Startup>();
+            using var client = application.CreateClient();
+
+            var formContent = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("username", "John Doe"),
+                new KeyValuePair<string, string>("password", "TeSimpel"),
+                new KeyValuePair<string, string>("email", "JohnDoe@example.com")
+            });
+
+            var response = await client.PostAsync("/api/authentication/register", formContent);
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
         }
     }
 }
